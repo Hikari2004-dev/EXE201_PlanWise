5802eb578d78f717a9c4d234f0243cae8664c55d
Hikari2004-dev <khanhkhoi08@gmail.com>
2026-07-16 15:56:56 +0700

    Revert "Update GoalAIController.java"
    This reverts commit da4210dd2d62778107e6f491a3c0d7db5c0855da.

--- be/src/main/java/com/exe201/planwise/ai/controller/GoalAIController.java
+++ be/src/main/java/com/exe201/planwise/ai/controller/GoalAIController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("legacyGoalAIController")
+@RestController
 @RequestMapping("/api/v1/ai/goals")
 @RequiredArgsConstructor
 public class GoalAIController {
